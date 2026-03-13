8d4816f7605f0aa94021686d58be334fe9ad7a63
Aadarshrai1801 <aadarshrai1801@gmail.com>
2026-03-13 18:55:51 +0530

    Second commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup #type: ignore
+from typing import List
+
+HYPHEN_E_DOT = "-e ."
+
+def get_requirements(file_path:str) -> List[str]: #type: ignore
+    '''
+    This function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+            
+    return requirements
+
+setup(
+    name = "ML-Development",
+    version = "0.0.1",
+    author = "Aadarsh",
+    author_email = "aadarshrai1801@gmail.com",
+    packages = find_packages(),
+    install_requires = get_requirements("requirements.txt")
+)
