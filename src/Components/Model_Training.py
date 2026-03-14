25b19b8970a0a3687c0fde69421cd5adeda02a5f
Aadarshrai1801 <aadarshrai1801@gmail.com>
2026-03-15 01:51:31 +0530

    Hypertuning done

--- src/Components/Model_Training.py
+++ src/Components/Model_Training.py
@@ -44,11 +44,67 @@ class ModelTrainer:
                 "Linear Regression" : LinearRegression(),
                 "K-Nieghbour Regressor" : KNeighborsRegressor(),
                 "XGB Regressor" : XGBRegressor(),
-                "Catboosting Regressor" : CatBoostRegressor(),
+                "Catboosting Regressor" : CatBoostRegressor(verbose=0),
                 "Adaboost Regressor" : AdaBoostRegressor()
             }
             
-            model_report:dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models)
+            params = {
+                "Random Forest": {
+                    "n_estimators": [100, 200, 500],
+                    "max_depth": [None, 10, 20, 30],
+                    "min_samples_split": [2, 5, 10],
+                    "min_samples_leaf": [1, 2, 4]
+                },
+
+                "Decision Tree": {
+                    "criterion": ["squared_error", "friedman_mse"],
+                    "splitter": ["best", "random"],
+                    "max_depth": [None, 10, 20, 30],
+                    "min_samples_split": [2, 5, 10]
+                },
+
+                "Gradient Boosting": {
+                    "learning_rate": [0.01, 0.05, 0.1],
+                    "n_estimators": [100, 200, 500],
+                    "subsample": [0.8, 1.0],
+                    "max_depth": [3, 5, 7]
+                },
+
+                "Linear Regression": {
+                    "fit_intercept": [True, False],
+                    "positive": [True, False]
+                },
+
+                "K-Nieghbour Regressor": {
+                    "n_neighbors": [3, 5, 7, 9],
+                    "weights": ["uniform", "distance"],
+                    "algorithm": ["auto", "ball_tree", "kd_tree"],
+                    "p": [1, 2]
+                },
+
+                "XGB Regressor": {
+                    "learning_rate": [0.01, 0.1, 0.2],
+                    "n_estimators": [100, 200, 500],
+                    "max_depth": [3, 5, 7],
+                    "subsample": [0.8, 1.0],
+                    "colsample_bytree": [0.8, 1.0]
+                },
+
+                "Catboosting Regressor": {
+                    "iterations": [100, 200, 500],
+                    "learning_rate": [0.01, 0.1, 0.2],
+                    "depth": [4, 6, 10],
+                    "l2_leaf_reg": [1, 3, 5]
+                },
+
+                "Adaboost Regressor": {
+                    "n_estimators": [50, 100, 200],
+                    "learning_rate": [0.01, 0.1, 1.0],
+                    "loss": ["linear", "square", "exponential"]
+                }
+            }
+            
+            model_report:dict = evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models, param=params)
             
             # To get the best model score from dict
             best_model_score = max(sorted(model_report.values()))
@@ -59,6 +115,7 @@ class ModelTrainer:
             ]
             
             best_model = models[best_model_name]
+            best_model.fit(X_train, y_train)
             
             if best_model_score < 0.6:
                 raise CustomException("No best model found", sys) #type: ignore
--- src/Utils.py
+++ src/Utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from Src.Exception import CustomException
 
@@ -18,23 +19,31 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys) #type: ignore
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         reports = {}
-        
-        for i in range(len(list(models))):
-            model = list(models.values())[i]
-            
-            model.fit(X_train, y_train)
-            
-            y_train_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
-            
+
+        for model_name, model in models.items():
+
+            param_grid = param[model_name]
+
+            gridsearchcv = GridSearchCV(model, param_grid=param_grid, cv=3)
+            gridsearchcv.fit(X_train, y_train)
+
+            best_model = gridsearchcv.best_estimator_
+
+            y_train_pred = best_model.predict(X_train)
+            y_test_pred = best_model.predict(X_test)
+
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
-            
-            reports[list(models.keys())[i]] = test_model_score
-        
+
+            reports[model_name] = test_model_score
+
         return reports
+
     except Exception as e:
         raise CustomException(e, sys) #type: ignore
