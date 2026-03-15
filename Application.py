a60698c2911d8d60ebf9b958d6569bf8c931e726
Aadarshrai1801 <aadarshrai1801@gmail.com>
2026-03-15 21:26:27 +0530

    error solved

--- Application.py
+++ Application.py
@@ -7,14 +7,13 @@ from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 application = Flask(__name__)
-app = application
 
 # Route for home page
-@app.route("/")
+@application.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/predict", methods=["GET", "POST"])
+@application.route("/predict", methods=["GET", "POST"])
 def predict_datapoint():
     if request.method == "GET":
         return render_template("home.html")
@@ -38,4 +37,4 @@ def predict_datapoint():
         return render_template("home.html", results=results[0])
     
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    application.run(host="0.0.0.0")
